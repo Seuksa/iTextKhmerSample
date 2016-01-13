91e32b7062a67a336a1f999e35db48b8e3d1270f
Pongsametrey Sok <metreysk@gmail.com>
2016-01-13 14:20:46 +0700

    Correct the encoding

--- src/main/java/org/seuksa/sample/khmeriText/App.java
+++ src/main/java/org/seuksa/sample/khmeriText/App.java
@@ -36,16 +36,16 @@ public class App
 
             KhmerLigaturizer d = new KhmerLigaturizer();
         	//String strProcess = d.process("\u0936\u093e\u0902\u0924\u094d\u093f");
-            String strProcess = "Hello " + d.process("ហេតុនេះ​យើង​ត្រូវ​តែរួម​គ្នា​ដោះ​ស្រាយ ខ្លួន ស្វាគមន៍មកកាន់អូស៊ីហ្វាយ");
+            String strProcess = d.process("ហេតុនេះ​យើង​ត្រូវ​តែរួម​គ្នា​ដោះ​ស្រាយ ខ្លួន ស្វាគមន៍មកកាន់អូស៊ីហ្វាយ");
 
             BaseFont bfComic = BaseFont.createFont(localFontPath, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
             //document.add(new Paragraph("Unicode: ការ​សិក្សា​ខាងបច្ចេក​វិទ្យា​នៅ​មាន​តម្រូវការខ្លាំង​លើ​ការ​ប្រើប្រាស់​ភាសា​ខ្មែរ", new Font(bfComic, 12)));
+            document.add(new Paragraph("Hello"));
             document.add(new Paragraph(strProcess, new Font(bfComic, 22)));
 
             ColumnText columnText = new ColumnText(writer.getDirectContent());
             columnText.setRunDirection(PdfWriter.RUN_DIRECTION_LTR);
             //columnText.addElement(new Paragraph("Unicode: ហេតុនេះ​យើង​ត្រូវ​តែរូម​គ្នា​ដោះ​ស្រាយ......", new Font(bfComic, 12)));
-
             document.add(new Paragraph(d.process("\u1780\u17B6\u179A\u200B\u179F\u17B7\u1780\u17D2\u179F\u17B6\u200B\u1781\u17B6\u1784\u1794\u1785\u17D2\u1785\u17C1\u1780\u200B\u179C\u17B7\u1791\u17D2\u1799\u17B6\u200B\u1793\u17C5\u200B\u1798\u17B6\u1793\u200B\u178F\u1798\u17D2\u179A\u17BC\u179C\u1780\u17B6\u179A\u1781\u17D2\u179B\u17B6\u17C6\u1784\u200B\u179B\u17BE\u200B\u1780\u17B6\u179A\u200B\u1794\u17D2"), new Font(bfComic, 12)));
 
         } catch (Exception e) {
